e023347de1fbc5d59f68cf5c89dc09859b79d3a9
Ivan Shiyan <ivan.shiyan@gmail.com>
2021-06-21 07:10:30 +0200

    ohlcv -> bar

---
+++ trading/data/entities/bar.go
@@ -0,0 +1,68 @@
+package entities
+
+import (
+	"fmt"
+	"time"
+)
+
+// Bar represents an [open, high, low, close, volume] price bar.
+type Bar struct {
+	// Time is the date and time of the closing price.
+	Time time.Time `json:"t"`
+
+	// Open is the opening price.
+	Open float64 `json:"o"`
+
+	// High is the highest price.
+	High float64 `json:"h"`
+
+	// Low is the lowest price.
+	Low float64 `json:"l"`
+
+	// Close is the closing price.
+	Close float64 `json:"c"`
+
+	// Volume is the aggregated volume.
+	Volume float64 `json:"v"`
+}
+
+// IsRising indicates whether this is a rising bar, i.e. the opening price is less than the closing price.
+func (b *Bar) IsRising() bool {
+	return b.Open < b.Close
+}
+
+// IsFalling indicates whether this is a falling bar, i.e. the closing price is less than the opening price.
+func (b *Bar) IsFalling() bool {
+	return b.Close < b.Open
+}
+
+// Median is the median price, calculated as
+//   (low + high) / 2.
+func (b *Bar) Median() float64 {
+	return (b.Low + b.High) / 2 //nolint:gomnd
+}
+
+// Typical is the typical price, calculated as
+//   (low + high + close) / 3.
+func (b *Bar) Typical() float64 {
+	return (b.Low + b.High + b.Close) / 3 //nolint:gomnd
+}
+
+// Weighted is the weighted price, calculated as
+//   (low + high + 2*close) / 4.
+func (b *Bar) Weighted() float64 {
+	return (b.Low + b.High + b.Close + b.Close) / 4 //nolint:gomnd
+}
+
+// Average is the weighted price, calculated as
+//   (low + high + open + close) / 4.
+func (b *Bar) Average() float64 {
+	return (b.Low + b.High + b.Open + b.Close) / 4 //nolint:gomnd
+}
+
+const timeFmt = "2006-01-02 15:04:05"
+
+// String implements the Stringer interface.
+func (b *Bar) String() string {
+	return fmt.Sprintf("{%s, %f, %f, %f, %f, %f}", b.Time.Format(timeFmt), b.Open, b.High, b.Low, b.Close, b.Volume)
+}
---
+++ trading/data/entities/bar_component.go
@@ -0,0 +1,101 @@
+package entities
+
+import (
+	"errors"
+	"fmt"
+)
+
+// BarComponent defines a component of the Bar type.
+type BarComponent int
+
+// BarFunc defines a function to get a component value from the Bar type.
+type BarFunc func(b *Bar) float64
+
+const (
+	// BarOpenPrice is the opening price component.
+	BarOpenPrice BarComponent = iota
+
+	// BarHighPrice is the highest price component.
+	BarHighPrice
+
+	// BarLowPrice is the lowest price component.
+	BarLowPrice
+
+	// BarClosePrice is the closing price component.
+	BarClosePrice
+
+	// BarVolume is the volume component.
+	BarVolume
+
+	// BarMedianPrice is the median price component, calculated as
+	//   (low + high) / 2.
+	BarMedianPrice
+
+	// BarTypicalPrice is the typical price component, calculated as
+	//   (low + high + close) / 3.
+	BarTypicalPrice
+
+	// BarWeightedPrice is the weighted price component, calculated as
+	//   (low + high + 2*close) / 4.
+	BarWeightedPrice
+
+	// BarAveragePrice is the average price component, calculated as
+	//   (low + high + open + close) / 4.
+	BarAveragePrice
+)
+
+var errUnknownBarComponent = errors.New("unknown bar component")
+
+//nolint:cyclop
+// BarComponentFunc returns an BarFunc function to get a component value from the Bar type.
+func BarComponentFunc(c BarComponent) (BarFunc, error) {
+	switch c {
+	case BarOpenPrice:
+		return func(b *Bar) float64 { return b.Open }, nil
+	case BarHighPrice:
+		return func(b *Bar) float64 { return b.High }, nil
+	case BarLowPrice:
+		return func(b *Bar) float64 { return b.Low }, nil
+	case BarClosePrice:
+		return func(b *Bar) float64 { return b.Close }, nil
+	case BarVolume:
+		return func(b *Bar) float64 { return b.Volume }, nil
+	case BarMedianPrice:
+		return func(b *Bar) float64 { return b.Median() }, nil
+	case BarTypicalPrice:
+		return func(b *Bar) float64 { return b.Typical() }, nil
+	case BarWeightedPrice:
+		return func(b *Bar) float64 { return b.Weighted() }, nil
+	case BarAveragePrice:
+		return func(b *Bar) float64 { return b.Average() }, nil
+	default:
+		return nil, fmt.Errorf("%d: %w", int(c), errUnknownBarComponent)
+	}
+}
+
+//nolint:cyclop
+// String implements the Stringer interface.
+func (c BarComponent) String() string {
+	switch c {
+	case BarOpenPrice:
+		return "OpenPrice"
+	case BarHighPrice:
+		return "HighPrice"
+	case BarLowPrice:
+		return "LowPrice"
+	case BarClosePrice:
+		return "ClosePrice"
+	case BarVolume:
+		return "Volume"
+	case BarMedianPrice:
+		return "MedianPrice"
+	case BarTypicalPrice:
+		return "TypicalPrice"
+	case BarWeightedPrice:
+		return "WeightedPrice"
+	case BarAveragePrice:
+		return "AveragePrice"
+	default:
+		return fmt.Errorf("%d: %w", int(c), errUnknownBarComponent).Error()
+	}
+}
