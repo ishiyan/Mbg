adaee03fb4cdbb56ab65b05632cf0a210ba9b695
ishiyan <ivan.shiyan@gmail.com>
2025-03-18 20:34:47 +0100

    kama

--- trading/indicators/kaufman/kaufmanadaptivemovingaverageparams.go
+++ trading/indicators/kaufman/kaufmanadaptivemovingaverageparams.go
@@ -4,16 +4,55 @@ import "mbg/trading/data"
 
 // KaufmanAdaptiveMovingAverageLengthParams describes parameters to create an instance of the indicator
 // based on length.
+//
+// Kaufman's adaptive moving average (KAMA) is an EMA with the smoothing factor, α,
+// being changed with each new sample within the fastest and the slowest boundaries.
+//
+// KAMAᵢ = αPᵢ + (1 - α)*KAMAᵢ₋₁,  α = (αs + (αf - αs)ε)²
+//
+// where the αf is the α of the fastest (shortest, default 2 samples) period boundary,
+// the αs is the α of the slowest (longest, default 30 samples) period boundary,
+// and ε is the efficiency ratio:
+//
+// ε = |P - Pℓ| / ∑|Pᵢ - Pᵢ₊₁|,  i ≤ ℓ-1
+//
+// where ℓ is a number of samples used to calculate the ε.
+// The recommended values of ℓ are in the range of 8 to 10.
+//
+// The efficiency ratio has the value of 1 when samples move in the same direction for
+// the full ℓ periods, and a value of 0 when samples are unchanged over the ℓ periods.
+// When samples move in wide swings within the interval, the sum of the denominator
+// becomes very large compared with the numerator and the ε approaches 0.
+// Smaller values of ε result in a smaller smoothing constant and a slower trend.
+//
+// The indicator is not primed during the first ℓ updates.
+//
+// See
+// Perry J. Kaufman, Smarter Trading, McGraw-Hill, Ney York, 1995, pp. 129-153
+// for a complete discussion.
 type KaufmanAdaptiveMovingAverageLengthParams struct {
-	// Length is the length (the number of time periods, ℓ) of the moving window to calculate the average.
+	// EfficiencyRatioLength is the number of last samples used to calculate the efficiency ratio.
 	//
 	// The value should be greater than 1.
-	Length int
+	EfficiencyRatioLength int
 
-	// FirstIsAverage indicates whether the very first exponential moving average value is
-	// a simple average of the first 'period' (the most widely documented approach) or
-	// the first input value (used in Metastock).
-	FirstIsAverage bool
+	// FastestLength is the fastest boundary length, ℓf.
+	// The equivalent smoothing factor αf is
+	//
+	//   αf = 2/(ℓf + 1), 2 ≤ ℓ
+	//
+	// The value should be greater than 1.
+	// The default value is 2.
+	FastestLength int
+
+	// SlowestLength is the slowest boundary length, ℓs.
+	// The equivalent smoothing factor αs is
+	//
+	//   αs = 2/(ℓs + 1), 2 ≤ ℓ
+	//
+	// The value should be greater than 1.
+	// The default value is 30.
+	SlowestLength int
 
 	// BarComponent indicates the component of a bar to use when updating the indicator with a bar sample.
 	BarComponent data.BarComponent
